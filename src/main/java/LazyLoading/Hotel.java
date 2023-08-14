c129101b32ec659a645ef07d1af2b7595b20138a
Krishna <krishna.paliwal85@gmail.com>
2023-08-14 18:49:22 +0530

    hibernate-oneToMany

---
+++ src/main/java/LazyLoading/Hotel.java
@@ -0,0 +1,52 @@
+package LazyLoading;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    private List<Room> rooms = new ArrayList<Room>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Room> getRooms() {
+		return rooms;
+	}
+
+	public void setRooms(List<Room> rooms) {
+		this.rooms = rooms;
+	}
+
+    
+}
